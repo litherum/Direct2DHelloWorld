8765e6ccb97510cf033b5285a2a63bfad300c74d
Myles C. Maxfield <mmaxfield@apple.com>
2015-12-16 22:24:14 -0800

    Initial Commit

---
+++ 11On12Test/Common/DeviceResources.h
@@ -0,0 +1,110 @@
+﻿#pragma once
+
+namespace DX
+{
+	static const UINT c_frameCount = 3;		// Use triple buffering.
+
+	// Controls all the DirectX device resources.
+	class DeviceResources
+	{
+	public:
+		DeviceResources();
+		void SetWindow(Windows::UI::Core::CoreWindow^ window);
+		void SetLogicalSize(Windows::Foundation::Size logicalSize);
+		void SetCurrentOrientation(Windows::Graphics::Display::DisplayOrientations currentOrientation);
+		void SetDpi(float dpi);
+		void ValidateDevice();
+		void Present();
+		void WaitForGpu();
+
+		// The size of the render target, in pixels.
+		Windows::Foundation::Size	GetOutputSize() const				{ return m_outputSize; }
+
+		// The size of the render target, in dips.
+		Windows::Foundation::Size	GetLogicalSize() const				{ return m_logicalSize; }
+
+		float						GetDpi() const						{ return m_effectiveDpi; }
+		bool						IsDeviceRemoved() const				{ return m_deviceRemoved; }
+
+		// D3D Accessors.
+		ID3D12Device*				GetD3DDevice() const				{ return m_d3dDevice.Get(); }
+		ID3D11On12Device*			GetD3D11On12Device() const			{ return m_d3d11On12Device.Get(); }
+		ID3D11DeviceContext*		GetD3D11DeviceContext() const		{ return m_d3d11DeviceContext.Get(); }
+		ID2D1DeviceContext2*		GetD2DDeviceContext() const			{ return m_d2dDeviceContext.Get(); }
+		IDWriteFactory*				GetDWriteFactory() const			{ return m_dWriteFactory.Get(); }
+		IDXGISwapChain3*			GetSwapChain() const				{ return m_swapChain.Get(); }
+		ID3D12Resource*				GetRenderTarget() const				{ return m_renderTargets[m_currentFrame].Get(); }
+		ID3D11Resource*				GetWrappedBackBuffer() const		{ return m_wrappedBackBuffers[m_currentFrame].Get(); }
+		ID2D1Bitmap1*				GetD2DRenderTarget() const			{ return m_d2dRenderTargets[m_currentFrame].Get(); }
+		ID3D12Resource*				GetDepthStencil() const				{ return m_depthStencil.Get(); }
+		ID3D12CommandQueue*			GetCommandQueue() const				{ return m_commandQueue.Get(); }
+		ID3D12CommandAllocator*		GetCommandAllocator() const			{ return m_commandAllocators[m_currentFrame].Get(); }
+		D3D12_VIEWPORT				GetScreenViewport() const			{ return m_screenViewport; }
+		DirectX::XMFLOAT4X4			GetOrientationTransform3D() const	{ return m_orientationTransform3D; }
+		UINT						GetCurrentFrameIndex() const		{ return m_currentFrame; }
+
+		CD3DX12_CPU_DESCRIPTOR_HANDLE GetRenderTargetView() const
+		{
+			return CD3DX12_CPU_DESCRIPTOR_HANDLE(m_rtvHeap->GetCPUDescriptorHandleForHeapStart(), m_currentFrame, m_rtvDescriptorSize);
+		}
+		CD3DX12_CPU_DESCRIPTOR_HANDLE GetDepthStencilView() const
+		{
+			return CD3DX12_CPU_DESCRIPTOR_HANDLE(m_dsvHeap->GetCPUDescriptorHandleForHeapStart());
+		}
+
+	private:
+		void CreateDeviceIndependentResources();
+		void CreateDeviceResources();
+		void CreateWindowSizeDependentResources();
+		void UpdateRenderTargetSize();
+		void MoveToNextFrame();
+		DXGI_MODE_ROTATION ComputeDisplayRotation();
+		void GetHardwareAdapter(IDXGIFactory4* pFactory, IDXGIAdapter1** ppAdapter);
+
+		UINT											m_currentFrame;
+
+		// Direct3D objects.
+		Microsoft::WRL::ComPtr<ID3D12Device>			m_d3dDevice;
+		Microsoft::WRL::ComPtr<ID3D11On12Device>		m_d3d11On12Device;
+		Microsoft::WRL::ComPtr<ID3D11DeviceContext>		m_d3d11DeviceContext;
+		Microsoft::WRL::ComPtr<ID2D1Factory3>			m_d2dFactory;
+		Microsoft::WRL::ComPtr<ID2D1Device2>			m_d2dDevice;
+		Microsoft::WRL::ComPtr<ID2D1DeviceContext2>		m_d2dDeviceContext;
+		Microsoft::WRL::ComPtr<IDWriteFactory>			m_dWriteFactory;
+		Microsoft::WRL::ComPtr<IDXGIFactory4>			m_dxgiFactory;
+		Microsoft::WRL::ComPtr<IDXGISwapChain3>			m_swapChain;
+		Microsoft::WRL::ComPtr<ID3D12Resource>			m_renderTargets[c_frameCount];
+		Microsoft::WRL::ComPtr<ID3D11Resource>			m_wrappedBackBuffers[c_frameCount];
+		Microsoft::WRL::ComPtr<ID2D1Bitmap1>			m_d2dRenderTargets[c_frameCount];
+		Microsoft::WRL::ComPtr<ID3D12Resource>			m_depthStencil;
+		Microsoft::WRL::ComPtr<ID3D12DescriptorHeap>	m_rtvHeap;
+		Microsoft::WRL::ComPtr<ID3D12DescriptorHeap>	m_dsvHeap;
+		UINT											m_rtvDescriptorSize;
+		Microsoft::WRL::ComPtr<ID3D12CommandQueue>		m_commandQueue;
+		Microsoft::WRL::ComPtr<ID3D12CommandAllocator>	m_commandAllocators[c_frameCount];
+		D3D12_VIEWPORT									m_screenViewport;
+		bool											m_deviceRemoved;
+
+		// CPU/GPU Synchronization.
+		Microsoft::WRL::ComPtr<ID3D12Fence>				m_fence;
+		UINT64											m_fenceValues[c_frameCount];
+		HANDLE											m_fenceEvent;
+
+		// Cached reference to the Window.
+		Platform::Agile<Windows::UI::Core::CoreWindow>	m_window;
+
+		// Cached device properties.
+		Windows::Foundation::Size						m_d3dRenderTargetSize;
+		Windows::Foundation::Size						m_outputSize;
+		Windows::Foundation::Size						m_logicalSize;
+		Windows::Graphics::Display::DisplayOrientations	m_nativeOrientation;
+		Windows::Graphics::Display::DisplayOrientations	m_currentOrientation;
+		float											m_dpi;
+
+		// This is the DPI that will be reported back to the app. It takes into account whether the app supports high resolution screens or not.
+		float											m_effectiveDpi;
+
+		// Transforms used for display orientation.
+		DirectX::XMFLOAT4X4								m_orientationTransform3D;
+	};
+}
